7623ae0af00798cb63a3e084b4bcf2b69479baf9
Greccu <crscrs352@gmail.com>
2021-04-13 17:27:31 +0300

    Create SQLQuery4.sql

---
+++ DB/DBDB/SQLQuery4.sql
@@ -0,0 +1,24 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Id]
+      ,[Nume]
+  FROM [DBDB].[dbo].[Facultati]
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Id]
+      ,[StudentId]
+      ,[CursId]
+  FROM [DBDB].[dbo].[StudentCurs]
+
+  /****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Id]
+      ,[Nume]
+      ,[Prenume]
+      ,[Varsta]
+      ,[FacultateId]
+  FROM [DBDB].[dbo].[Studenti]
+
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Id]
+      ,[Nume]
+  FROM [DBDB].[dbo].[Cursuri]
+
